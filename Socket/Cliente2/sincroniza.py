fa13da13089930615bf391f4c837ca3c81dd174e
JeanKaria <102553889+Jibabk@users.noreply.github.com>
2023-06-16 14:09:51 -0300

    Add files via upload

--- Socket/Cliente2/sincroniza.py
+++ Socket/Cliente2/sincroniza.py
@@ -2,20 +2,25 @@ import zipfile
 import os
 import datetime
 import sys
-import shutil
 
 def sincronize(changes):
-    if changes:
+    for change in changes:
+        caminho = ''
+        for letter in change:
+            if letter == "\\":
+                letter = "/"
+            caminho += letter
         with zipfile.ZipFile(os.path.join(sys.path[0], 'recive\ZipArquivo.zip'), 'r') as zip_ref:
-            zip_ref.extractall(os.path.join(sys.path[0], 'FolderToSinc'))
+            zip_ref.extract(caminho, os.path.join(sys.path[0], 'FolderToSinc'))
+
 
 
 
 RecivertimeList = []
-for path,dirs,files in os.walk('FolderToSink'):
+for path,dirs,files in os.walk(os.path.join(sys.path[0], 'FolderToSinc')):
     for file in files:
         filename = os.path.join(path,file)
-        relpath = os.path.relpath(filename,'FolderToSink')
+        relpath = os.path.relpath(filename,os.path.join(sys.path[0], 'FolderToSinc'))
         filesize = os.path.getsize(filename)
         # create a file path
         #print(relpath)
@@ -29,15 +34,15 @@ for path,dirs,files in os.walk('FolderToSink'):
             # file creation timestamp in float
             c_time = os.path.getctime(path)
             # convert creation timestamp into DateTime object
-            dt_c = datetime.datetime.fromtimestamp(c_time)        
+            dt_c = datetime.datetime.fromtimestamp(c_time)       
 
             RecivertimeList.append([str(relpath),str(dt_c),str(dt_m)])
         except:
             continue
 
-
+#print(RecivertimeList)
 senderTimeList = []
-print(os.getcwd())
+#print(os.getcwd())
 with open(os.path.join(sys.path[0], "SenderTime.txt"), "r") as arq:   
     data = arq.readlines()
     for i in data:
@@ -45,19 +50,27 @@ with open(os.path.join(sys.path[0], "SenderTime.txt"), "r") as arq:
         aux[2]=aux[2][:-1]
         senderTimeList.append(aux)
 
-    print(senderTimeList)
+    #print(senderTimeList)
     
+#print('aaaaaaaaa')
+#print(senderTimeList[0][0])
 changes = []
+#print(RecivertimeList)
+#print(senderTimeList)
+#print('bbbbbbbbb')
+
 for dateSender in senderTimeList:
     flag = False
     for dateReciver in RecivertimeList:
         if dateSender[0] == dateReciver[0]:
             flag = True
-            if datetime.datetime.strptime(dateSender[2], '%Y-%m-%d %H:%M:%S.%f') <= datetime.datetime.strptime(dateReciver[2], '%Y-%m-%d %H:%M:%S.%f'):
+            #print(f"dateSender:{dateSender[2]},DateReciver:{dateReciver[2]}")
+            if datetime.datetime.strptime(dateSender[2], '%Y-%m-%d %H:%M:%S.%f') >= datetime.datetime.strptime(dateReciver[2], '%Y-%m-%d %H:%M:%S.%f'):
                 changes.append(dateReciver[0])
     if not flag:
         changes.append(dateSender[0])
 
+#for i in changes: print(i)
 
 sincronize(changes)
 
